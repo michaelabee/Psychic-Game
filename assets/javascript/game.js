8f810fc4d2ab48b153f0695d8e9ede51aa3e9166
Michaela Banczak <m.a.banczak@gmail.com>
2019-04-21 18:09:50 -0500

    added javascript for computer guesses and variables for user guesses/game stats

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,26 @@
-var alphabet = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+alert ("You have 10 guesses! What letter am I thinking of?")
+
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var randomLetter = alphabet[Math.floor(Math.random()*alphabet.length)];
+console.log(randomLetter);
 
+var userGuess = document.onkeyup = function(event){
+
+}
+
+var wins = 0
+
+var losses = 0
+
+var guesses = 10
+
+
+if (userGuess === randomLetter){
+    wins ++;
+}
+else {guesses --
+}; 
+
+if (guesses === 0);
+    {losses ++};
