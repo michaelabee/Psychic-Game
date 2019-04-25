258b2446c6d886e1039c7d8b9788bd3d2cf85781
Michaela Banczak <m.a.banczak@gmail.com>
2019-04-25 10:22:42 -0500

    fixed functions and created neccessary arrays

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,28 @@ alert ("You have 10 guesses! What letter am I thinking of?")
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var wins = 0;
+
+var losses = 0;
+
+var guesses = 10;
+
 var randomLetter = alphabet[Math.floor(Math.random()*alphabet.length)];
 console.log(randomLetter);
 
-var userGuess = document.onkeyup = function(event){
+var userGuess = $(document).keyup(function(event){
+        console.log(userGuess);
+    });
 
-}
 
-var wins = 0
 
-var losses = 0
+$("#guess").text(userGuess);
 
-var guesses = 10
+$("#win").text(wins);
 
+$("#lose").text(losses);
+
+$("#guess-left").text(guesses);
 
 if (userGuess === randomLetter){
     wins ++;
