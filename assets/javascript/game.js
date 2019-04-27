1068bfe27049aa4a014bc1cc1452f29a3df2deb4
Michaela Banczak <m.a.banczak@gmail.com>
2019-04-26 21:17:43 -0500

    added css and fixed some js elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-alert ("You have 10 guesses! What letter am I thinking of?")
+// alert ("You have 10 guesses! What letter am I thinking of?")
 
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -11,25 +11,42 @@ var guesses = 10;
 var randomLetter = alphabet[Math.floor(Math.random()*alphabet.length)];
 console.log(randomLetter);
 
-var userGuess = $(document).keyup(function(event){
-        console.log(userGuess);
-    });
+var userGuess = []
+
+document.onkeyup = function(event){
+    console.log(event);
+
+    guesses --;
+
+    userGuess.push(event.key);
+
+    $("#guess").text(userGuess);
+
+    $("#win").text(wins);
+
+    $("#lose").text(losses);
+
+    $("#guess-left").text(guesses);
+
+    if (guesses === 0) {
+        losses ++;
+        guesses = 10;
+        //pick a new random letter here
+        //reset userGuess array here
+    };
+    if (randomLetter === userGuess){
+        wins++;
+        guesses = 10;
+        //pick a new random letter here
+        //reset userGuess array here
+    }
+
+};
+
+// //if (randomLetter === userGuess){
+//     wins ++
+// }; 
 
 
 
-$("#guess").text(userGuess);
 
-$("#win").text(wins);
-
-$("#lose").text(losses);
-
-$("#guess-left").text(guesses);
-
-if (userGuess === randomLetter){
-    wins ++;
-}
-else {guesses --
-}; 
-
-if (guesses === 0);
-    {losses ++};
